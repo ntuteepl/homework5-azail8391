5d11fd6341805197d35d8be0fc73032827b420cd
azail8391 <146065149+azail8391@users.noreply.github.com>
2023-10-31 15:20:08 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+main()
+{
+int  f,a;
+float j;
+scanf("%d%d",&f,&a);
+
+ if (f<=60){
+    j=f*a;
+
+    printf ("%.1f\n",j);
+
+
+ }
+ else if (f>60&&f<=120){
+   j =60*a;
+    j=j+(f-60)*a*1.33;
+    printf ("%.1f\n",j);
+
+ }
+  else {
+    j=60*a;
+    j=j+60*a*1.33;
+    j=j+(f-120)*a*1.66;
+    printf ("%.1f\n",j);
+  }
+}
 
